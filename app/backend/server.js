88d75cc9f0ad92b7ac29bdc56b59b8b2f978dc1e
ricardokuhlkamp <ric.kds@hotmail.com>
2023-08-06 01:39:34 -0300

    update server.js with api

--- app/backend/server.js
+++ app/backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
-const routes = require("./routes/index");
+const routes = require("./src/routes/index");
 
 const cors = require("cors");
 
@@ -11,18 +11,18 @@ const PORT = process.env.PORT | 5000;
 app.use(express.json());
 
 // Defina a origem permitida no corsOptions
-const corsOptions = {
-  origin: ["https://books-realm-app.vercel.app"],
-  methods: ["POST", "GET", "DELETE", "PUT"],
-  credentials: true
-};
+// const corsOptions = {
+//   origin: ["https://books-realm-app.vercel.app"],
+//   methods: ["POST", "GET", "DELETE", "PUT"],
+//   credentials: true
+// };
 
-app.use(cors(corsOptions));
-// app.use(cors());
+// app.use(cors(corsOptions));
+app.use(cors());
 
-// app.get("/", (req, res) => {
-//   res.send("ok...");
-// });
+app.get("/", (req, res) => {
+  res.send("ok...");
+});
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
